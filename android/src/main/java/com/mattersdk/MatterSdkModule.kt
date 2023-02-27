82d360d7812b88f82addac871a63551583f14837
qimi <15217710611@163.com>
2023-02-27 11:45:44 +0800

    style: 优化Android代码格式

--- android/src/main/java/com/mattersdk/MatterSdkModule.kt
+++ android/src/main/java/com/mattersdk/MatterSdkModule.kt
@@ -6,20 +6,20 @@ import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
 
 class MatterSdkModule(reactContext: ReactApplicationContext) :
-  ReactContextBaseJavaModule(reactContext) {
+    ReactContextBaseJavaModule(reactContext) {
 
-  override fun getName(): String {
-    return NAME
-  }
+    override fun getName(): String {
+        return NAME
+    }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  @ReactMethod
-  fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
-  }
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    fun multiply(a: Double, b: Double, promise: Promise) {
+        promise.resolve(a * b)
+    }
 
-  companion object {
-    const val NAME = "MatterSdk"
-  }
+    companion object {
+        const val NAME = "MatterSdk"
+    }
 }
