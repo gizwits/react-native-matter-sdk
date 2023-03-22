2dd2454592de6ee352435594eca68549eddc3b0f
qimi <15217710611@163.com>
2023-03-22 15:19:12 +0800

    fix: 完善原始设备配对码解析接口

--- android/src/main/java/com/gizwits/matter/sdk/core/model/MatterSetupPayload.kt
+++ android/src/main/java/com/gizwits/matter/sdk/core/model/MatterSetupPayload.kt
@@ -6,8 +6,10 @@ data class MatterSetupPayload(
     val version: Int,
     val vendorId: Int,
     val productId: Int,
+    val commissioningFlow: Int,
     val discoveryCapabilities: List<String>,
     val discriminator: Int,
+    val hasShortDiscriminator: Boolean,
     val setupPinCode: Long
 )
 
@@ -16,10 +18,12 @@ fun SetupPayload.asMatterSetupPayload(): MatterSetupPayload =
         version = version,
         vendorId = vendorId,
         productId = productId,
+        commissioningFlow = commissioningFlow,
         discoveryCapabilities = discoveryCapabilities.map {
             it.name
         },
         discriminator = discriminator,
+        hasShortDiscriminator = hasShortDiscriminator,
         setupPinCode = setupPinCode
     )
 
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { pairDeviceWithAddress } from 'react-native-matter-sdk';
+import { parseForSetupPayload } from 'react-native-matter-sdk';
 
 export default function App() {
   const [message, setMessage] = React.useState<string | undefined>();
 
   React.useEffect(() => {
-    // 通过parseManualCodeForSetupPayload解析配对码，获取设备配对信息
-    pairDeviceWithAddress('1', 3840, '20202021').then(setMessage);
+    
   }, []);
 
   return (
